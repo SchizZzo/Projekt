d86bdf2048b82a8ef4a46504f72bed8f398edf36
SchizZzo <83632210+SchizZzo@users.noreply.github.com>
2025-10-13 17:07:13 +0200

    Add DRF login API endpoint

--- BACKEND/django/joker_projekt/login/views.py
+++ BACKEND/django/joker_projekt/login/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.authtoken.models import Token
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from .serializers import LoginSerializer
+
+
+class LoginAPIView(APIView):
+    """API endpoint that allows users to obtain an auth token."""
+
+    permission_classes = [AllowAny]
+
+    def post(self, request, *args, **kwargs):
+        serializer = LoginSerializer(data=request.data, context={"request": request})
+        serializer.is_valid(raise_exception=True)
+
+        user = serializer.validated_data["user"]
+        token, _ = Token.objects.get_or_create(user=user)
+
+        return Response({"token": token.key}, status=status.HTTP_200_OK)
